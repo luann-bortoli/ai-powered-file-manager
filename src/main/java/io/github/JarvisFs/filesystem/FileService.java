ae65a3f0b635337c75f56b8afefe12d26289ece3
Luann <luanndebortoli@gmail.com>
2026-05-27 20:45:49 -0300

    fix: resolve merge conflicts

--- src/main/java/io/github/JarvisFs/filesystem/FileService.java
+++ src/main/java/io/github/JarvisFs/filesystem/FileService.java
@@ -11,148 +11,88 @@ import java.util.Comparator;
 @Service
 public class FileService {
 
-    static String BASE_URL = "C:/JARVIS_SANDBOX";
+    private static final Path BASE_PATH = Paths.get("C:/JARVIS_SANDBOX")
+            .toAbsolutePath()
+            .normalize();
 
-<<<<<<< HEAD
     private static final Logger logger = LoggerFactory.getLogger(FileService.class);
 
-    public Path resolveSafePath(String input){
-=======
-    public Path resolveSafePath(String input){
+    private Path resolveSafePath(String input) {
 
-        Path base = Paths.get(BASE_URL).normalize().toAbsolutePath();
-
-        Path targetPath = base
+        Path targetPath = BASE_PATH
                 .resolve(input)
                 .normalize()
                 .toAbsolutePath();
 
-        if (!targetPath.startsWith(base)){
+        if (!targetPath.startsWith(BASE_PATH)) {
+
             System.out.println("[-] Access outside default sandbox requested.");
+            logger.warn("Attempted access outside sandbox: {}", input);
+
             throw new SecurityException("Access outside sandbox requested");
         }
 
         return targetPath;
-
-    };
-
-    public void createFolder(String name) throws IOException{
-
-        Path targetPath = resolveSafePath(name);
-
-        Files.createDirectories(targetPath);
->>>>>>> b145a293c2c69805308563256a570d6573a9c0e5
-
-        Path base = Paths.get(BASE_URL).normalize().toAbsolutePath();
-
-        Path targetPath = base
-                .resolve(input)
-                .normalize()
-                .toAbsolutePath();
-
-<<<<<<< HEAD
-        if (!targetPath.startsWith(base)){
-            System.out.println("[-] Access outside default sandbox requested.");
-            logger.warn("Access outside default sandbox requested.");
-            throw new SecurityException("Access outside sandbox requested");
-        }
-
-        return targetPath;
-=======
-    public void createFile(String name, String content) throws IOException{
-
-        Path targetPath = resolveSafePath(name);
-
-        Files.writeString(targetPath, content, StandardOpenOption.CREATE);
-
-        System.out.println("[+] File " + name + " created successfully!\n");
-
     }
 
-    public void delete(String name) throws IOException{
-
-        Path targetPath = resolveSafePath(name);
-
-        if(Files.notExists(targetPath)){
-            throw new RuntimeException("Path not found: " + targetPath);
-        }
-
-        if(Files.isDirectory(targetPath)){
-            Files.walk(targetPath)
-                    .sorted(Comparator.reverseOrder())
-                    .forEach(path -> {
-                        try {
-                            Files.delete(path);
-                        } catch (IOException e){
-                            System.out.println(e.getMessage());
-                        }
-                    });
->>>>>>> b145a293c2c69805308563256a570d6573a9c0e5
-
-    }
-
-    public void createFolder(String name){
+    public void createFolder(String name) {
 
         logger.info("Creating folder: {}", name);
 
         Path targetPath = resolveSafePath(name);
 
-        try
-        {
+        try {
+
             Files.createDirectories(targetPath);
 
             System.out.println("[+] Folder " + name + " created successfully!\n");
             logger.info("Folder created successfully: {}", name);
 
-        } catch (IOException e){
+        } catch (IOException e) {
 
             System.out.println("Error creating folder: " + e.getMessage());
             logger.error("Error creating folder: {}", name, e);
-
         }
     }
 
-    public void createFile(String name, String content){
+    public void createFile(String name, String content) {
 
         logger.info("Creating file: {}", name);
 
         Path targetPath = resolveSafePath(name);
 
-        try
-        {
+        try {
             Files.writeString(targetPath, content, StandardOpenOption.CREATE);
 
             System.out.println("[+] File " + name + " created successfully!\n");
             logger.info("File created successfully: {}", name);
 
-        } catch (IOException e){
+        } catch (IOException e) {
 
             System.out.println("Error creating file: " + e.getMessage());
             logger.error("Error creating file: {}", name, e);
-
         }
-
-
     }
 
-    public void delete(String name){
+    public void delete(String name) {
 
         logger.info("Deleting file/folder: {}", name);
 
         Path targetPath = resolveSafePath(name);
 
-        if(Files.notExists(targetPath)){
-            logger.error("Path not found : {}", targetPath);
+        if (Files.notExists(targetPath)) {
+
+            logger.error("Path not found: {}", targetPath);
             throw new RuntimeException("Path not found: " + targetPath);
         }
 
-        if(Files.isDirectory(targetPath)) {
+        if (Files.isDirectory(targetPath)) {
 
-            try (var paths = Files.walk(targetPath)){
+            try (var paths = Files.walk(targetPath)) {
 
-                    paths
-                        .sorted(Comparator.reverseOrder())
+                paths.sorted(Comparator.reverseOrder())
                         .forEach(path -> {
+
                             try {
 
                                 Files.delete(path);
@@ -161,7 +101,6 @@ public class FileService {
 
                                 System.out.println(e.getMessage());
                                 logger.error("Error deleting file: {}", path, e);
-
                             }
                         });
 
@@ -170,102 +109,94 @@ public class FileService {
 
             } catch (IOException e) {
 
-                System.out.println("Error deleting file: " + e.getMessage());
-                logger.error("Error deleting file: {}", name, e);
-
+                System.out.println("Error deleting folder: " + e.getMessage());
+                logger.error("Error deleting folder: {}", name, e);
             }
 
             return;
         }
 
-<<<<<<< HEAD
-        try
-        {
+        try {
+
             Files.delete(targetPath);
-=======
-        Files.delete(targetPath);
->>>>>>> b145a293c2c69805308563256a570d6573a9c0e5
 
             System.out.println("[+] File " + name + " deleted successfully!\n");
             logger.info("File deleted successfully: {}", name);
 
-        } catch (IOException e){
+        } catch (IOException e) {
 
             System.out.println("Error deleting file: " + e.getMessage());
-            logger.info("Error deleting file: {}", e.getMessage());
-
+            logger.error("Error deleting file: {}", name, e);
         }
-
     }
 
-    public void rename(String name, String newName){
+    public void rename(String name, String newName) {
 
-<<<<<<< HEAD
         logger.info("Renaming file/folder: {}", name);
 
-=======
->>>>>>> b145a293c2c69805308563256a570d6573a9c0e5
         Path currentPath = resolveSafePath(name);
-
         Path newPath = resolveSafePath(newName);
 
-        if(Files.notExists(currentPath)){
-            logger.error("Path not found : {}", currentPath);
+        if (Files.notExists(currentPath)) {
+
+            logger.error("Path not found: {}", currentPath);
+
             throw new RuntimeException("Path not found: " + currentPath);
-        }
-
-        try
-        {
-            Files.move(currentPath, newPath, StandardCopyOption.REPLACE_EXISTING);
-
-            System.out.println("[+] File " + name + " renamed to " + newName + " successfully!\n");
-            logger.info("File {} renamed successfully to: {}", name, newName);
-
-        } catch (IOException e){
-
-            System.out.println("Error renaming file: " + e.getMessage());
-            logger.error("Error renaming file: {}", e.getMessage());
-
-        }
-    }
-
-    public void list(String name){
-
-<<<<<<< HEAD
-        logger.info("Listing files: {}", name);
-
-=======
->>>>>>> b145a293c2c69805308563256a570d6573a9c0e5
-        Path targetPath = resolveSafePath(name);
-
-        if(Files.isRegularFile(targetPath)) {
-            System.out.println(targetPath.getFileName());
-<<<<<<< HEAD
-            logger.info("File {} listed successfully!\n", name);
-            return;
-        }
-
-        try (var paths = Files.walk(targetPath)){
-
-                paths
-=======
-            return;
         }
 
         try {
 
-            Files.walk(targetPath)
->>>>>>> b145a293c2c69805308563256a570d6573a9c0e5
-                    .forEach(path -> {
-                        System.out.println(path.getFileName());
-                    });
-            logger.info("Folder {} listed successfully!\n", name);
+            Files.move(currentPath, newPath, StandardCopyOption.REPLACE_EXISTING);
 
-        } catch (IOException e){
+            if (Files.isRegularFile(newPath)) {
+
+                System.out.println("[+] File " + name + " renamed to " + newName + " successfully!\n");
+
+                logger.info("File {} renamed successfully to {}", name, newName);
+
+            } else {
+
+                System.out.println("[+] Folder " + name + " renamed to " + newName + " successfully!\n");
+
+                logger.info("Folder {} renamed successfully to {}", name, newName);
+            }
+
+        } catch (IOException e) {
+
+            System.out.println("Error renaming file/folder: " + e.getMessage());
+
+            logger.error("Error renaming file/folder: {}", name, e);
+        }
+    }
+
+    public void list(String name) {
+
+        logger.info("Listing files: {}", name);
+
+        Path targetPath = resolveSafePath(name);
+
+        if (Files.isRegularFile(targetPath)) {
+
+            System.out.println(targetPath.getFileName());
+
+            logger.info("File listed successfully: {}", name);
+
+            return;
+        }
+
+        try (var paths = Files.walk(targetPath)) {
+
+            paths.forEach(path -> {
+                System.out.println(path.getFileName());
+            });
+
+            logger.info("Folder listed successfully: {}", name);
+
+        } catch (IOException e) {
 
             System.out.println(e.getMessage());
-            logger.error("Error in listing files : {}", e.getMessage());
 
+            logger.error("Error listing files: {}", name, e);
         }
     }
 }
